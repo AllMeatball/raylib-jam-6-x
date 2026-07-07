2ae501aac8801b593404ccc444ab60e0324f50c7
AllMeatball <181806857+AllMeatball@users.noreply.github.com>
2026-07-07 15:15:48 -0500

    cjspoly: Add filename for function constructor

--- js/modules/cjspoly.js
+++ js/modules/cjspoly.js
@@ -3,13 +3,18 @@ class CommonModule {
 }
 
 export function require(path) {
+    // const module_name = path;
+
     if (path.startsWith('./') || path.startsWith('../'))
         path = FS_AbsolutePath("js/"+path);
 
     const code = RL_LoadFileText(path);
 
     const module = new CommonModule();
-    Function('module', 'exports', code).apply(null, [module, module.exports]);
+    const func = Function('module', 'exports', code, path);
+
+    func.apply(null, [module, module.exports]);
+
     // console.log(module.exports, exports);
 
     return module.exports;
