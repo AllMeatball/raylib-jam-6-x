1e016b4c2fee553e94b561ce776ee98e6ec283a1
AllMeatball <181806857+AllMeatball@users.noreply.github.com>
2026-07-08 16:54:33 -0500

    Fix web build and add global flag support

--- res/js/patterns.js
+++ res/js/patterns.js
@@ -7,17 +7,24 @@ class PatternSystem {
     doPattern(pattern) {
         for (const action of pattern) {
             const result = action(this);
-
             queue.push(result);
         }
     }
 
     update(dt) {
         const tmp_queue = [...this.queue];
-        for (const item of tmp_queue) {
+
+        for (let i = 0; i < tmp_queue.length; i++) {
+            const item = tmp_queue[i];
+
             item.timer -= dt;
-            if (item.timer < 0)
-                this.queue.splice();
+            if (item.timer < 0) {
+                ENTITIES.push(...item.entities);
+                this.queue.splice(i);
+                continue;
+            }
+
+            break;
         }
     }
 }
